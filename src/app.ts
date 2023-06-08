7b137ef81cf2bea40e525e8e4130da6421d3659e
Dinesh Kumar <dinesh@rocketsource.co>
2023-06-08 18:16:02 +0530

    Added role and schema

--- src/app.ts
+++ src/app.ts
@@ -5,30 +5,57 @@ const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
-
-app.get('/users', async (req: Request, res: Response) => {
+app.get('/roles', async (req: Request, res: Response) => {
   try {
-    const users = await prisma.user.findMany();
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
+    const roles = await prisma.user.findMany();
+    res.json(roles);
+  } catch (err) {
+    res.status(500).json({ error: err });
   }
 });
 
-app.post('/users', async (req: Request, res: Response) => {
-  try {
-    const { name, email } = req.body;
-    const user = await prisma.user.create({
-      data: {
-        name,
-        email,
-      },
-    });
-    res.json(user);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
+app.post('/roles', async (req: Request, res: Response) => {
+  // Create new role
 });
+
+app.get('/roles/{id}', async (req: Request, res: Response) => {
+  // get role  by id
+});
+
+app.put('/roles/{id}', async (req: Request, res: Response) => {
+  // update role  by id
+});
+app.delete('/roles/{id}', async (req: Request, res: Response) => {
+  // delete role  by id
+});
+
+// app.get('/users', async (req: Request, res: Response) => {
+//   try {
+//     const users = await prisma.user.findMany();
+//     res.json(users);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// });
+
+// app.post('/users', async (req: Request, res: Response) => {
+//   try {
+//     const { name, email } = req.body;
+//     const user = await prisma.user.create({
+//       data: {
+//         name,
+//         email,
+//       },
+//       include: {
+//         role: true
+//       }
+//     });
+//     res.json(user);
+//     //user.role.name
+//   } catch (error) {
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// });
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
